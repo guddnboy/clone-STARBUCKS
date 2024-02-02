907977dd681bd421270059d0814e04147eb9346d
guddnboy <hung3018@naver.com>
2024-02-02 15:18:54 +0900

    feat : hover 시 메뉴 펼치기 (미완성)

---
+++ js/starbucks.js
@@ -0,0 +1,7 @@
+
+const menu = document.querySelector('.hidden-menu-menu');
+const store = document.querySelector('.hidden-menu-store');
+const response = document.querySelector('.hidden-menu-response');
+const reward = document.querySelector('.hidden-menu-reward');
+const sales = document.querySelector('.hidden-menu-sales');
+const new_menu = document.querySelector('.hidden-menu-new');
