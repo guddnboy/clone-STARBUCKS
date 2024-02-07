c32f62ea826fbc2e5e05404263417a40c8fc9e81
guddnboy <hung3018@naver.com>
2024-02-07 21:16:04 +0900

    feat: 메뉴 창 바로 안사라지게 수정

--- js/starbucks.js
+++ js/starbucks.js
@@ -11,33 +11,45 @@ let cnt = 0;
 const coffee_menu = document.querySelector('#coffee-menu');
 coffee_menu.addEventListener('mouseover', showCoffee);
 coffee_menu.addEventListener('mouseout', hideCoffee);
+coffee.addEventListener('mouseover', showCoffee);
+coffee.addEventListener('mouseout', hideCoffee);
 
 const menu_menu = document.querySelector('#menu-menu');
 menu_menu.addEventListener('mouseover', showMenu);
 menu_menu.addEventListener('mouseout', hideMenu);
+menu.addEventListener('mouseover', showMenu);
+menu.addEventListener('mouseout', hideMenu);
 
 const store_menu = document.querySelector('#store-menu');
 store_menu.addEventListener('mouseover', showStore);
 store_menu.addEventListener('mouseout', hideStore);
+store.addEventListener('mouseover', showStore);
+store.addEventListener('mouseout', hideStore);
 
 
 const response_menu = document.querySelector('#response-menu');
 response_menu.addEventListener('mouseover', showResponse);
 response_menu.addEventListener('mouseout', hideResponse);
+response.addEventListener('mouseover', showResponse);
+response.addEventListener('mouseout', hideResponse);
 
 const reward_menu = document.querySelector('#reward-menu');
 reward_menu.addEventListener('mouseover', showReward);
 reward_menu.addEventListener('mouseout', hideReward);
+reward.addEventListener('mouseover', showReward);
+reward.addEventListener('mouseout', hideReward);
 
 const sales_menu = document.querySelector('#sales-menu');
 sales_menu.addEventListener('mouseover', showSales);
 sales_menu.addEventListener('mouseout', hideSales);
-
+sales.addEventListener('mouseover', showSales);
+sales.addEventListener('mouseout', hideSales);
 
 const new_menu_menu = document.querySelector('#new-menu');
 new_menu_menu.addEventListener('mouseover', showNew);
 new_menu_menu.addEventListener('mouseout', hideNew);
-
+new_menu.addEventListener('mouseover', showNew);
+new_menu.addEventListener('mouseout', hideNew);
 
 function showCoffee(){
     coffee.style.display = 'block';
